da9ab581314ac52a1998f7ac72400d6dabf0f30e
Hamid_mp <mohamadali.mahmoodpour@gmail.com>
2023-06-24 01:59:00 +0330

    update Classification

--- Classification.py
+++ Classification.py
@@ -4,21 +4,50 @@ import torchvision
 
 from torchvision.models import vgg16, resnet50, inception_v3
 from torch.utils.data import DataLoader
+from configparser import ConfigParser
+from pathlib import Path
+
+path = Path.cwd().joinpath('Configs.ini')
+configs = ConfigParser(  )
+configs.read(path)
+
+
+
+#(configs['Training']['Epochs']))
 
 
 
 
 class Classification():
 
-    def __init__(self, Data_Loader=None, phase='Train', model_name='vgg'):
+    def __init__(self, Data_Loader=None, phase='Train'):
 
         assert isinstance(Data_Loader, DataLoader), 'Data must be a DataLoader object'
         self.data = Data_Loader
 
-        self.model_name = model_name
+
     
     def Fine_Tune(self):
-        if self.model_name in 
+        
+        model_name = configs['Training']['model']
+
+        if model_name == 'vgg16':
+            model = vgg16(pretrained=True)
+
+        elif model_name == 'resnet50':
+            model = resnet50(pretrained=True)
+        
+        elif model_name == 'inception_v3':
+            model = inception_v3(pretrained=True)
+
+        else:
+            raise ValueError
+
+        
+            
+
+        
+        
 
 
 
