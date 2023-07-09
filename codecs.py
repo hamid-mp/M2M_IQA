d6bc95db517d8bb3f50fadb7765b8c49fcc1b4f9
Hamid_mp <mohamadali.mahmoodpour@gmail.com>
2023-07-10 00:08:50 +0330

    delete unnecessary file

--- codecs.py
+++
@@ -1,7 +0,0 @@
-import cv2
-from imagecodecs import jpeg2k_encode
-import numpy
-
-array = numpy.random.randint(100, 200, (256, 256, 3), numpy.uint8)
-encoded = jpeg2k_encode(array, level=0)
-print(bytes(encoded[:12]))
